11ffeec8ab00f19229563c7ac29adc4cd6af9732
amber46 <amber46@gmail.com>
2020-05-10 09:40:02 +0300

    Changes more lines

--- make_disks.bash
+++ make_disks.bash
@@ -2,8 +2,10 @@
 #
 
 DST="/mnt/dst"
+SRCIP="37"
 
 # step 1
+parted /dev/sda mklabel msdos
 parted -a optimal /dev/sda mkpart primary ext4 0% 512M
 parted /dev/sda set 1 boot on
 parted -a optimal /dev/sda mkpart primary ext4 512M 100%
@@ -12,28 +14,30 @@ mkfs.ext4 /dev/sda1 -L BOOT
 mkfs.ext4 /dev/sda2 -L SYSTEM
 
 # step 2
-mkdir $DST
+mkdir -p $DST
 mount -L SYSTEM $DST
-mount -L BOOT $DST/boot/
+mkdir -p $DST/boot
+mount -L BOOT $DST/boot
 
 
 # step 5
-rsync -ahPHAXx --delete --exclude={/dev/*,/proc/*,/sys/*,/tmp/*,/run/*,/mnt/*,/media/*,/var/log/android/*,/home/vpn_android/data/*,/lost+found} -e "ssh -p 1111" root@source-ip:/ /mnt/dst/
-
+rsync -ahPHAXx --delete --exclude={/dev/*,/proc/*,/sys/*,/tmp/*,/var/tmp/*,/run/*,/mnt/*,/media/*,/var/log/android/*,/home/vpn_android/data/*,/lost+found} -e "ssh -p 1111" root@$SRCIP:/ /mnt/dst/
 
 # step 9
 MACADDR_ETH0=$(cat /sys/class/net/eth0/address)
 MACADDR_VGN0=$(cat /sys/class/net/eth1/address)
 
-echo 'SUBSYSTEM=="net", ACTION=="add", ATTR{address}=="$MACADDR_ETH0",KERNEL=="*", NAME="eth0"
-SUBSYSTEM=="net", ACTION=="add", ATTR{address}=="$MACADDR_VGN0",KERNEL=="*", NAME="vgn0'
- >> $DST/etc/udev/rules.d/10-network.rules
+cat <<EOT > $DST/etc/udev/rules.d/10-network.rules
+SUBSYSTEM=="net", ACTION=="add", ATTR{address}=="$MACADDR_ETH0",KERNEL=="*", NAME="eth0"
+SUBSYSTEM=="net", ACTION=="add", ATTR{address}=="$MACADDR_VGN0",KERNEL=="*", NAME="vgn0"
+EOT
 
-echo '# / was on /dev/sda2 during installation
-LABEL=SYSTEM   /               ext4    errors=remount-ro 0       1
+cat <<EOT > $DST/etc/fstab
+# / was on /dev/sda2 during installation
+LABEL=SYSTEM / ext4 errors=remount-ro 0 1
 # /boot was on /dev/sda1 during installation
-LABEL=BOOT     /boot           ext4    defaults          0       2'
- >> $DST/etc/fstab
+LABEL=BOOT /boot ext4 defaults 0 2
+EOT
 
 # step 10 
 mount --rbind /dev $DST/dev
