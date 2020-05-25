7ca624be8d6ac3cffe779986a18d9e0b6e0aeae4
amber46 <amber46@gmail.com>
2020-05-25 16:47:01 +0300

    ????????????? ?????????

--- sync_proxy.bash
+++ sync_proxy.bash
@@ -3,30 +3,35 @@
 
 SRCIP=$1
 DST="/mnt/dst"
+#/dev/sda:
 #/boot = 512M
-#/ = 12G
-#/home = 4G
-#/var/log = 4G
+#/ = 16G
+
+#/dev/sdb:
 #/var/riproxy = 8G
+
+#/dev/sdc:
+#/var/log = 8G
 
 # STEP 1
 parted /dev/sda mklabel msdos
-parted -a optimal /dev/sda mkpart primary ext4 0% 512M
+parted -a optimal /dev/sda mkpart primary ext4 0% 512M $ /boot
 parted /dev/sda set 1 boot on
-parted -a optimal /dev/sda mkpart primary ext4 512M 12800M # /
-parted -a optimal /dev/sda mkpart primary ext4 12800M 16896M # /home
-parted -a optimal /dev/sda mkpart primary ext4 16896M 100% #20992M # /var/log
+parted -a optimal /dev/sda mkpart primary ext4 512M 100% # /
+
 parted /dev/sdb mklabel msdos
-parted -a optimal /dev/sdb mkpart primary ext4 0% 100%
+parted -a optimal /dev/sdb mkpart primary ext4 0% 100% #/var/riproxy
+
+parted /dev/sdc mklabel msdos
+parted -a optimal /dev/sdc mkpart primary ext4 0% 100% # /var/log
 
 #parted /dev/sda unit s p free
 mkfs.ext4 /dev/sda1 -L BOOT
 mkfs.ext4 /dev/sda2 -L SYSTEM
-mkfs.ext4 /dev/sda3 -L HOME
-mkfs.ext4 /dev/sda4 -L LOG
 mkfs.reiserfs /dev/sdb1 -l RIPROXY
-# mount disks to /mnt
+mkfs.ext4 /dev/sdc1 -L LOG
 
+# mount disks to /mnt
 mkdir -p $DST
 mount -L SYSTEM $DST
 mkdir -p $DST/boot
@@ -40,8 +45,10 @@ mount -L RIPROXY $DST/var/riproxy
 
 # STEP 2
 rsync -ahPHAXx --delete    --exclude={/dev/*,/proc/*,/sys/*,/tmp/*,/run/*,/var/tmp/*,/mnt/*,/media/*,/var/log/boot-repair,/var/log/**.gz,/var/log/**.1,/var/riproxy/users_memory_data/*,/lost+found} -e "ssh -p 1111" root@$SRCIP:/ $DST/
+
 # repair missing folder
 rsync -av -f"+ */" -f"- *" --exclude={/dev/*,/proc/*,/sys/*,/tmp/*,/run/*,/var/tmp/*,/mnt/*,/media/*,/lost+found} -e "ssh -p 1111" root@$SRCIP:/ $DST/
+
 # nullify log files
 find /mnt/dst/var/log/ -type f -exec truncate -s 0 {} \;
 find /mnt/dst/var/www/vpn/log/ -type f -exec truncate -s 0 {} \;
@@ -65,11 +72,14 @@ mount --rbind /dev $DST/dev
 mount -t proc /proc $DST/proc
 mount --rbind /sys $DST/sys
 mount --rbind /tmp $DST/tmp
+
 cat <<EOT
+##### CHROOT ######
 chroot $DST /bin/bash
 
-#Correct GRUB
+##### Correct GRUB ######
 grub-install --no-floppy /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
+
 #possible need set use_lvmetad = 0 in /etc/lvm/lvm.conf
 EOT
--- sync_vpn.bash
+++ sync_vpn.bash
@@ -11,8 +11,10 @@ parted -a optimal /dev/sda mkpart primary ext4 0% 512M
 parted /dev/sda set 1 boot on
 parted -a optimal /dev/sda mkpart primary ext4 512M 100%
 #parted /dev/sda unit s p free
+
 mkfs.ext4 /dev/sda1 -L BOOT
 mkfs.ext4 /dev/sda2 -L SYSTEM
+
 # mount disks to /mnt
 mkdir -p $DST
 mount -L SYSTEM $DST
@@ -21,8 +23,10 @@ mount -L BOOT $DST/boot
 
 # STEP 2
 rsync -ahPHAXx --delete    --exclude={/dev/*,/proc/*,/sys/*,/tmp/*,/run/*,/var/tmp/*,/mnt/*,/media/*,/var/log/boot-repair,/var/log/**.gz,/var/log/**.1,/var/log/android/*,/home/vpn_android/data/*,/lost+found} -e "ssh -p 1111" root@$SRCIP:/ $DST/
+
 # repair missing folder
 rsync -av -f"+ */" -f"- *" --exclude={/dev/*,/proc/*,/sys/*,/tmp/*,/run/*,/var/tmp/*,/mnt/*,/media/*,/var/log/boot-repair,/lost+found} -e "ssh -p 1111" root@$SRCIP:/ $DST/
+
 # nullify log files
 find /mnt/dst/var/log/ -type f -exec truncate -s 0 {} \;
 find /mnt/dst/var/www/vpn/log/ -type f -exec truncate -s 0 {} \;
@@ -46,6 +50,7 @@ mount --rbind /dev $DST/dev
 mount -t proc /proc $DST/proc
 mount --rbind /sys $DST/sys
 mount --rbind /tmp $DST/tmp
+
 cat <<EOT
 chroot $DST /bin/bash
 
